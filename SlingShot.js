41b60371d56ed3f449c60a9fac38297e044b16dd
A-S-Arjith <arjith.arya@gmail.com>
2021-01-26 17:22:11 +0900

    Add slingshot and bye bye pigs

--- SlingShot.js
+++ SlingShot.js
@@ -9,19 +9,41 @@ class SlingShot{
 
         this.sling= Constraint.create(options);
         this.pointB=point2;
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
         World.add(world,this.sling); 
 }
 
 fly(){
     this.sling.bodyA=null;
 }
+
+attach(body1){
+    this.sling.bodyA=body1;
+}
     display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,10);
+    push();
         if(this.sling.bodyA){
             var pointA=this.sling.bodyA.position;
             var pointB=this.pointB;
-            strokeWeight(4);
-            line(pointA.x,pointA.y,pointB.x,pointB.y);
+            stroke("#301608");
+            if(pointA.x>220){
+                strokeWeight(4);
+                line(pointA.x+25,pointA.y,pointB.x-10,pointB.y);
+                line(pointA.x+25,pointA.y,pointB.x+25,pointB.y);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
+            else{
+                strokeWeight(8);
+                line(pointA.x-25,pointA.y,pointB.x-10,pointB.y);
+                line(pointA.x-25,pointA.y,pointB.x+25,pointB.y);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }  
         }
+        pop();
         
     }
 }
